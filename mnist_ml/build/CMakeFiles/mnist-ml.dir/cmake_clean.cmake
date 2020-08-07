42b29eba3c807f0b43d7486d45d8e26ca428409c
gOps132 <epilred@icloud.com>
2020-08-07 21:16:34 +0800

    succesfully exracted data

---
+++ mnist_ml/build/CMakeFiles/mnist-ml.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnist-ml.dir/src/data.cpp.o"
+  "CMakeFiles/mnist-ml.dir/src/data_handler.cpp.o"
+  "mnist-ml"
+  "mnist-ml-1.0.0"
+  "mnist-ml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnist-ml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
